0d3109ed7539ef78a8aac2df10cd0fb30616d5d9
desmonty <36315338+desmonty@users.noreply.github.com>
2018-07-11 14:35:04 -0400

    Add files via upload

--- source/main.d
+++ source/main.d
@@ -4,14 +4,35 @@ import std.algorithm: copy, map;
 import std.datetime;
 import std.complex;
 import std.functional: toDelegate;
+import std.getopt;
 import std.math;
 import std.random;
 import std.stdio : writeln;
 
 import source.Layer;
 import source.Matrix;
-//import source.NeuralNetwork;
+import source.NeuralNetwork;
 import source.Parameter;
 import source.Utils;
 
-void main(){}
+import source.optimizers.random_search;
+
+/// GLOBALS
+
+auto optimizers_tests = false;
+
+///
+
+
+void main(string[] args) {
+    auto optinfo = getopt(args,
+        "optimizers_tests", &optimizers_tests
+    );
+
+    if (optimizers_tests)
+        run_optimizers_tests();
+}
+
+void run_optimizers_tests() {
+    random_search_tests();
+}
