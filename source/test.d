2ff2a7ff2429b108ed8ad5f60e2674e5de00c60a
desmonty <36315338+desmonty@users.noreply.github.com>
2018-11-21 14:16:42 -0500

    +bias
    +Add bias layer array in neural network
    - change extract_name to extract_property

--- source/test.d
+++ source/test.d
@@ -1,23 +1,26 @@
-/+ Function used exclusively to extract the names of the layers and
- + return them in the form of a array.stringof
- +
- + Ideally, we need to find a we to generalize it to extract _any_
- + piece of information cntained in the layers.
+import std.conv: to;
+
+
+/+ Function used to extract any given property of the layers
+ + and return them in the form of a to!string(string[])
  +/
-auto extract_name(args...)()
+auto extract_property(string property, args...)()
 {
     string unfolded_args= "[";
 
     static foreach(i; 0 .. args.length)
     {
-        unfolded_args ~= "\"";
-        unfolded_args ~= args[i].name;
-        unfolded_args ~= "\",";
+        mixin("static if (is(typeof(args[i]."~property~") :  string)) unfolded_args ~= \"\\\"\";");
+        mixin("unfolded_args ~= to!string(args[i]."~property~");");
+        mixin("static if (is(typeof(args[i]."~property~") :  string)) unfolded_args ~= \"\\\"\";");
+        static if (i < args.length-1)
+            unfolded_args ~= ",";
     }
 
     unfolded_args ~= "]";
     return unfolded_args;
 }
+
 
 /+ The shape of our Layers !
  + They will be templated struct.
@@ -26,19 +29,39 @@ auto extract_name(args...)()
  + the template arguments passed to the layer.
  +
  +/
-struct Linear(string _name)
+struct Linear(size_t _dim_out=0,
+              bool _bias=false,
+              string _name=null,
+              alias _state=null,
+              string[] _layers_in=null,
+              string[] _layers_out=null)
 {
+    static immutable size_t dim_out = _dim_out;
+    static immutable bool bias = _bias;
     static immutable string name = _name;
+    static immutable typeof(_state) state = _state;
+    static immutable string[] layers_in = _layers_in;
+    static immutable string[] layers_out = _layers_out;
 }
+
 
 /+ Neural Network class
  +/
-class NeuralNetwork(Layers...)
+class NeuralNetwork(typeInput, size_t dimInput, Layers...)
 {
     // Array of layers' name.
-    mixin("static immutable string[Layers.length] layers_names = "
-         ~extract_name!(Layers) 
-         ~";");
+    mixin(
+        "static immutable string[Layers.length] layers_names = "
+        ~extract_property!("name",Layers)
+        ~";"
+    );
+
+    // Array of layers' bias existance.
+    mixin(
+        "static immutable bool[Layers.length] layers_bias = "
+        ~extract_property!("bias",Layers)
+        ~";"
+    );
 
     this()
     {
@@ -49,11 +72,14 @@ class NeuralNetwork(Layers...)
 void main(string[] args)
 {
     static immutable auto nn = new NeuralNetwork!(
-        Linear!("Blue"),
-        Linear!("Unicorn"),
+        real,
+        10,
+        Linear!(0, false, "Blue", [0, 1, 2], [], ["Unicorn"]),
+        Linear!(0, false, "Unicorn", [0, 1, 2], ["Blue"], []),
     )();
 
     pragma(msg, nn.layers_names);
+    pragma(msg, nn.layers_bias);
     static assert(nn.layers_names[0] == "Blue");
     assert(nn.layers_names[1] == "Unicorn");
 }
